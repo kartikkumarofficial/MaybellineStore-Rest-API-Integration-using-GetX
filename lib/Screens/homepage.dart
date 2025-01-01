932d3a26e615d862156e2cf754b80f59404a6f29
kartikkumarofficial <work.kartikkumar@gmail.com>
2025-01-01 22:56:29 +0530

    structure

---
+++ lib/Screens/homepage.dart
@@ -0,0 +1,70 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+
+class homePage extends StatefulWidget {
+  homePage({super.key});
+
+  @override
+  State<homePage> createState() => _homePageState();
+}
+
+class _homePageState extends State<homePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(" Shopping App",
+          style: TextStyle(
+              fontWeight: FontWeight.w600,
+              fontSize: 26),),
+        actions: [IconButton(onPressed: (){}, icon: Icon(Icons.view_list_rounded)),
+                  IconButton(onPressed: (){}, icon: Icon(Icons.grid_view))
+        ],
+      ),
+      body: Column(
+        children: [
+
+          SizedBox(
+            height: 10,
+          ),
+
+
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.only(left: 7,right: 7),
+              child: GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2, // Number of columns in the grid
+                  crossAxisSpacing: 8.0, // Horizontal spacing between items
+                  mainAxisSpacing: 8.0, // Vertical spacing between items
+                ),
+                itemCount: 10, // Total number of items to display
+                itemBuilder: (BuildContext context, int index) {
+                  return Container(
+                    height: 100,
+                    width: 100,
+                    color: Colors.red,
+                    child: Center(child: Text('Item $index')),
+                  );
+                },
+              ),
+            ),
+          )
+
+
+
+
+
+
+
+
+
+
+
+
+        ],
+      )
+    );
+  }
+}
