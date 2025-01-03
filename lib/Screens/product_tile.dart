bd52608e354886f4de006a58a770b68b67e2dcc4
kartikkumarofficial <work.kartikkumar@gmail.com>
2025-01-03 14:46:48 +0530

    structure

--- lib/Screens/product_tile.dart
+++ lib/Screens/product_tile.dart
@@ -11,43 +11,45 @@ class ProductTile extends StatelessWidget {
       elevation: 2,
       child: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              height: 168,
-              width: double.infinity,
-              clipBehavior: Clip.antiAlias,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(4),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                height: 168,
+                width: double.infinity,
+                clipBehavior: Clip.antiAlias,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(4),
+                ),
+                child: Image.network(
+                  product.imageLink,
+                  fit: BoxFit.cover,
+                ),
               ),
-              child: Image.network(
-                product.imageLink,
-                fit: BoxFit.cover,
+              SizedBox(height: 8),
+              Text(
+                product.name,
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+                style:
+                TextStyle(fontFamily: 'avenir', fontWeight: FontWeight.w800),
               ),
-            ),
-            SizedBox(height: 8),
-            Text(
-              product.name,
-              maxLines: 2,
-              overflow: TextOverflow.ellipsis,
-              style:
-              TextStyle(fontFamily: 'avenir', fontWeight: FontWeight.w800),
-            ),
-            SizedBox(height: 8),
-            if (product.rating != null)
-              Row(
-                children: [
-                  Icon(Icons.star, color: Colors.yellow),
-                  Text(product.rating.toString()),
-                ],
+              SizedBox(height: 8),
+              if (product.rating != null)
+                Row(
+                  children: [
+                    Icon(Icons.star, color: Colors.yellow),
+                    Text(product.rating.toString()),
+                  ],
+                ),
+              SizedBox(height: 8),
+              Text(
+                '\$${product.price}',
+                style: TextStyle(fontSize: 24, fontFamily: 'avenir'),
               ),
-            SizedBox(height: 8),
-            Text(
-              '\$${product.price}',
-              style: TextStyle(fontSize: 24, fontFamily: 'avenir'),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
